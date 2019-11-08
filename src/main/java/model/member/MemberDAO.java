431699a39dce39de526dbd2189eea4bcbc708820
jungyoon huh <gjwjddbsg@gmail.com>
2019-11-08 18:45:11 +0900

    mypage

--- src/main/java/model/member/MemberDAO.java
+++ src/main/java/model/member/MemberDAO.java
@@ -118,6 +118,44 @@ public class MemberDAO {
         }
     }
 
+    //마이페이지 정보를 얻기 위한
+    public String getMemberLoc(String email){
+        getConnection();
+        String location = null;
+        try {
+            String sql = "select location from member where email = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,email);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                location = rs.getString(1);
+            }
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return location;
+    }
+
+    public String getMemberTel(String email){
+        getConnection();
+        String tel = null;
+        try {
+            String sql = "select tel from member where email = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,email);
+            rs = pstmt.executeQuery();
+            if(rs.next()){
+                tel = rs.getString(1);
+            }
+            conn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return tel;
+    }
+
+
 
 
 
