73b83fd06ef49cff93ec8975d1eb8ffe79bb0583
min <hsm012362@gmail.com>
2019-12-14 14:12:19 +0900

    commit

--- src/main/java/controller/product/urlSearch.java
+++ src/main/java/controller/product/urlSearch.java
@@ -1,11 +1,5 @@
 package controller.product;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class uslSearch extends HttpServlet {
+public class urlSearch extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
@@ -25,6 +19,5 @@ public class uslSearch extends HttpServlet {
         System.out.println("url : " + url);
         Process p = new Process();
         ArrayList<pInfo> plist =  p.Get(url);
-
     }
 }
